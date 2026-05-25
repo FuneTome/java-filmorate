44a0765b3ae47211108f799a158e5604987bd1c0
Fune_Tome <kurilenkof1905@yandex.ru>
2026-05-25 14:07:14 +0300

    добавлены рекомендации

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,121 +3,183 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dto.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class UserService {
     private final UserStorage userStorage;
+    private final GenreService genreService;
+    private final MpaService mpaService;
+    private final DirectorService directorService;
 
-    public UserService(@Qualifier("userDbStorage") UserStorage userStorage) {
+    public UserService(@Qualifier("userDbStorage") UserStorage userStorage,
+                       GenreService genreService,
+                       MpaService mpaService,
+                       DirectorService directorService) {
         this.userStorage = userStorage;
+        this.genreService = genreService;
+        this.mpaService = mpaService;
+        this.directorService = directorService;
     }
 
-    public Collection<User> getUsers() {
+    public Collection<UserDto> getUsers() {
         log.info("Запрос на получение всех пользователей");
-        return userStorage.getUsers().values();
+        return userStorage.getUsers().values().stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
     }
 
-    public User getUser(Long id) {
+    public UserDto getUser(Long id) {
         log.info("Запрос на получение пользователя по id: {}", id);
-        if (userStorage.findById(id)) {
-            return userStorage.getById(id);
+        if (!userStorage.findById(id)) {
+            log.warn("Пользователь с id {} не найден", id);
+            throw new NotFoundException("Юзер с id = " + id + " не найден");
         }
-        log.warn("Пользователь с id {} не найден", id);
-        throw new NotFoundException("Юзер с id = " + id + " не найден");
+        User user = userStorage.getById(id);
+        return toDto(user);
     }
 
-    public User addUser(User user) {
-        log.info("Запрос на добавление нового пользователя: {}", user.getLogin());
+    public UserDto addUser(UserRequest request) {
+        log.info("Запрос на добавление нового пользователя: {}", request.getLogin());
+        User user = toUser(request);
         isValid(user);
         if (user.getName() == null) {
             user.setName(user.getLogin());
         }
         User saved = userStorage.addUser(user);
         log.info("Пользователь успешно добавлен с id: {}", saved.getId());
-        return saved;
+        return toDto(saved);
     }
 
-    public User updateUser(User newUser) {
-        log.info("Запрос на обновление пользователя с id: {}", newUser.getId());
-        if (newUser.getId() == null) {
+    public UserDto updateUser(UserRequest request) {
+        log.info("Запрос на обновление пользователя с id: {}", request.getId());
+        if (request.getId() == null) {
             log.warn("Попытка обновления пользователя без указания id");
             throw new ValidationException("Id должен быть указан");
         }
-        if (!userStorage.findById(newUser.getId())) {
-            log.warn("Пользователь с id {} не найден для обновления", newUser.getId());
-            throw new NotFoundException("Юзер с id = " + newUser.getId() + " не найден");
+        if (!userStorage.findById(request.getId())) {
+            log.warn("Пользователь с id {} не найден для обновления", request.getId());
+            throw new NotFoundException("Юзер с id = " + request.getId() + " не найден");
         }
-        isValid(newUser);
-        User oldUser = userStorage.getById(newUser.getId());
-        User updated = userStorage.updateUser(oldUser, newUser);
+        User user = toUser(request);
+        user.setId(request.getId());
+        isValid(user);
+        User oldUser = userStorage.getById(request.getId());
+        User updated = userStorage.updateUser(oldUser, user);
         log.info("Пользователь с id {} успешно обновлён", updated.getId());
-        return updated;
+        return toDto(updated);
     }
 
-    public User addFriend(Long id, Long friendId) {
+    public UserDto addFriend(Long id, Long friendId) {
         log.info("Запрос на добавление друга id: {} пользователю id: {}", friendId, id);
-        if (!userStorage.findById(id)) {
-            log.warn("Пользователь id: {} не найден", id);
-            throw new NotFoundException("Юзер с id = " + id + " не найден");
-        }
-        if (!userStorage.findById(friendId)) {
-            log.warn("Друг id: {} не найден", friendId);
-            throw new NotFoundException("Юзер с id = " + friendId + " не найден");
-        }
+        checkUserExists(id);
+        checkUserExists(friendId);
         if (!userStorage.addFriend(id, friendId)) {
             log.warn("Пользователь id: {} уже в друзьях у id: {}", friendId, id);
             throw new NotFoundException("Такой человек уже есть в списке друзей");
         }
         log.info("Друг успешно добавлен");
-        return userStorage.getById(id);
+        User user = userStorage.getById(id);
+        return toDto(user);
     }
 
-    public Collection<User> getFriends(Long id) {
+    public Collection<UserDto> getFriends(Long id) {
         log.info("Запрос на получение друзей пользователя id: {}", id);
-        if (!userStorage.findById(id)) {
-            log.warn("Пользователь id: {} не найден", id);
-            throw new NotFoundException("Юзер с id = " + id + " не найден");
-        }
-        return userStorage.getFriends(id);
+        checkUserExists(id);
+        return userStorage.getFriends(id).stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
     }
 
-    public Collection<User> getCommonFriends(Long id, Long otherId) {
+    public Collection<UserDto> getCommonFriends(Long id, Long otherId) {
         log.info("Запрос на получение общих друзей пользователей id: {} и id: {}", id, otherId);
-        if (!userStorage.findById(id)) {
-            log.warn("Пользователь id: {} не найден", id);
-            throw new NotFoundException("Юзер с id = " + id + " не найден");
-        }
-        if (!userStorage.findById(otherId)) {
-            log.warn("Пользователь id: {} не найден", otherId);
-            throw new NotFoundException("Юзер с id = " + otherId + " не найден");
-        }
-        return userStorage.getCommonFriends(id, otherId);
+        checkUserExists(id);
+        checkUserExists(otherId);
+        return userStorage.getCommonFriends(id, otherId).stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
     }
 
     public void deleteFriend(Long id, Long friendId) {
         log.info("Запрос на удаление друга id: {} у пользователя id: {}", friendId, id);
-        if (!userStorage.findById(id)) {
-            log.warn("Пользователь id: {} не найден", id);
-            throw new NotFoundException("Юзер с id = " + id + " не найден");
-        }
-        if (!userStorage.findById(friendId)) {
-            log.warn("Друг id: {} не найден", friendId);
-            throw new NotFoundException("Юзер с id = " + friendId + " не найден");
-        }
+        checkUserExists(id);
+        checkUserExists(friendId);
         userStorage.removeFriend(id, friendId);
         log.info("Друг успешно удалён");
     }
 
+    public Collection<FilmDto> getRecommendations(Long id) {
+        log.info("Запрос на получение рекоммендации для пользователя с id: {}", id);
+        checkUserExists(id);
+        return userStorage.getRecommendations(id).stream()
+                .map(this::toFilmDto)
+                .collect(Collectors.toList());
+    }
+
+    private void checkUserExists(long userId) {
+        if (!userStorage.findById(userId)) {
+            log.warn("Пользователь с id {} не найден", userId);
+            throw new NotFoundException("Юзер с id = " + userId + " не найден");
+        }
+    }
+
+    private User toUser(UserRequest request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setLogin(request.getLogin());
+        user.setName(request.getName());
+        user.setBirthday(request.getBirthday());
+        return user;
+    }
+
+    private UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setEmail(user.getEmail());
+        dto.setLogin(user.getLogin());
+        dto.setName(user.getName());
+        dto.setBirthday(user.getBirthday());
+        return dto;
+    }
+
+    private FilmDto toFilmDto(Film film) {
+        FilmDto dto = new FilmDto();
+        dto.setId(film.getId());
+        dto.setName(film.getName());
+        dto.setDescription(film.getDescription());
+        dto.setReleaseDate(film.getReleaseDate());
+        dto.setDuration(film.getDuration());
+
+        if (film.getRating() != null) {
+            MpaDto mpaDto = mpaService.getRatingById(film.getRating().getId());
+            dto.setMpa(mpaDto);
+        }
+
+        List<GenreDto> genreDtos = film.getGenres().stream()
+                .map(genre -> genreService.getGenreById(genre.getId()))
+                .collect(Collectors.toList());
+        dto.setGenres(genreDtos);
+
+        List<DirectorDto> directorDtos = film.getDirector().stream()
+                .map(director -> directorService.getDirectorById(director.getId()))
+                .collect(Collectors.toList());
+        dto.setDirectors(directorDtos);
+        return dto;
+    }
+
     private void isValid(User user) {
-        if (user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday() == null) {
+        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
             log.warn("Некорректная дата рождения: {}", user.getBirthday());
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
