f8d717a81c7169aa050e3fcca8ab59af3a08559e
Ситников Сергей <sssitnikov@inbox.ru>
2026-05-26 00:15:34 +0300

    Решены конфликты

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -184,63 +184,6 @@ public class FilmService {
         }
     }
 
-    private Film toFilm(FilmRequest request) {
-        Film film = new Film();
-        film.setId(request.getId());
-        film.setName(request.getName());
-        film.setDescription(request.getDescription());
-        film.setReleaseDate(request.getReleaseDate());
-        film.setDuration(request.getDuration());
-        if (request.getMpa() != null) {
-            Rating rating = new Rating();
-            rating.setId(request.getMpa().getId());
-            film.setRating(rating);
-        }
-        if (request.getGenres() != null) {
-            Set<Genre> genres = request.getGenres().stream()
-                    .map(g -> {
-                        Genre genre = new Genre();
-                        genre.setId(g.getId());
-                        return genre;
-                    })
-                    .collect(Collectors.toSet());
-            film.setGenres(genres);
-        }
-        if (request.getDirectors() != null) {
-            Set<Director> directors = request.getDirectors().stream()
-                    .map(g -> {
-                        Director director = new Director();
-                        director.setId(g.getId());
-                        return director;
-                    })
-                    .collect(Collectors.toSet());
-            film.setDirector(directors);
-        }
-        return film;
-    }
-
-    private FilmDto toDto(Film film) {
-        FilmDto dto = new FilmDto();
-        dto.setId(film.getId());
-        dto.setName(film.getName());
-        dto.setDescription(film.getDescription());
-        dto.setReleaseDate(film.getReleaseDate());
-        dto.setDuration(film.getDuration());
-        if (film.getRating() != null) {
-            MpaDto mpaDto = mpaService.getRatingById(film.getRating().getId());
-            dto.setMpa(mpaDto);
-        }
-        List<GenreDto> genreDtos = film.getGenres().stream()
-                .map(genre -> genreService.getGenreById(genre.getId()))
-                .collect(Collectors.toList());
-        dto.setGenres(genreDtos);
-        List<DirectorDto> directorDtos = film.getDirector().stream()
-                .map(director -> directorService.getDirectorById(director.getId()))
-                .collect(Collectors.toList());
-        dto.setDirectors(directorDtos);
-        return dto;
-    }
-
     public void isValid(Film film) {
         if (film.getReleaseDate().isBefore(FIRST_FILM_DATE)) {
             log.warn("Дата релиза {} недопустима", film.getReleaseDate());
