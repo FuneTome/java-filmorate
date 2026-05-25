0180ea912d690c1a02ef31a1978f56dfc4aa7c22
Ситников Сергей <sssitnikov@inbox.ru>
2026-05-25 17:32:21 +0300

    Исправлен чекстайл окончательно

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -7,14 +7,24 @@ import java.util.List;
 import java.util.Map;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film oldFilm, Film newFilm);
+
     boolean findById(Long id);
+
     Film getById(Long id);
+
     Map<Long, Film> getFilms();
+
     boolean addLike(Long filmId, Long userId);
+
     boolean removeLike(Long filmId, Long userId);
+
     List<Film> getPopularFilms(int count);
+
     List<Film> getPopularFilms(int count, Integer genreId, Integer year);
+
     List<Film> getFilmsByDirector(long directorId, SortByOption sortBy);
 }
